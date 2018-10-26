dddc86ed02f2d2f06545cf0dc1bb068a3bbb82e4
Jorge Leal <jorgeae@gmail.com>
2018-10-26 14:15:49 -0300

    adiciona style ao card

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 const Card = ({ props }) => {
   return (
-    <div>
+    <div style={{ margin: '1em' }}>
       <img src="http://placehold.it/75" />
-      <div>Name</div>
-      <div>Company</div>
+      <div style={{ display: 'inline-block', marginLeft: 10 }}>
+        <div style={{ fontSize: '1.25em', fontWeight: 'bold' }}>Name</div>
+        <div>Company</div>
+      </div>
     </div>
   )
 }
