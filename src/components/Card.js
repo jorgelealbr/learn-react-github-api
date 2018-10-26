2a03d0ddbb6461b3eac9014d46580b545b6af25f
Jorge Leal <jorgeae@gmail.com>
2018-10-26 17:01:08 -0300

    adiciona antdesign

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Avatar } from 'antd'
 
 const Card = ({ props }) => {
   return (
     <div style={{ margin: '1em' }}>
-      <img src="http://placehold.it/75" />
+      <Avatar
+        size={75}
+        shape="square"
+        src="https://avatars3.githubusercontent.com/u/15630421?v=4"
+      />
       <div style={{ display: 'inline-block', marginLeft: 10 }}>
         <div style={{ fontSize: '1.25em', fontWeight: 'bold' }}>Name</div>
         <div>Company</div>
