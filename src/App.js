86d560ed3fe59cb67bf53336ce282da002f2a23b
Jorge Leal <jorgeae@gmail.com>
2018-10-27 02:43:32 -0300

    cardlist recebe a lista de cards via props

--- src/App.js
+++ src/App.js
@@ -3,9 +3,22 @@ import CardList from './components/CardList'
 
 class App extends Component {
   render() {
+    let data = [
+      {
+        name: 'Jorge Leal',
+        company: 'Thoughtworks BR',
+        avatar_url: 'https://avatars3.githubusercontent.com/u/15630421?v=4'
+      },
+      {
+        name: 'Aldenio',
+        company: 'BC',
+        avatar_url: 'https://avatars1.githubusercontent.com/u/7149450?v=4'
+      }
+    ]
+
     return (
       <div>
-        <CardList />
+        <CardList cards={data} />
       </div>
     )
   }
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react'
 import Card from './Card'
 
-const CardList = () => {
+const CardList = props => {
   return (
     <div>
-      <Card
-        name="Jorge Leal"
-        company="Thoughtworks"
-        avatar_url="https://avatars3.githubusercontent.com/u/15630421?v=4"
-      />
+      {props.cards.map(card => (
+        <Card {...card} />
+      ))}
     </div>
   )
 }
